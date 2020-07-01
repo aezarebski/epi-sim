427d925aa048e8008093f7d2785ea39a412f59d9
Alex Zarebski <aezarebski@gmail.com>
2020-07-01 11:37:43 +0100

    update bdscod example to use the newick output

--- examples/bdscod/Main.hs
+++ examples/bdscod/Main.hs
@@ -6,7 +6,6 @@ import qualified Data.ByteString.Builder as BBuilder
 import qualified Data.ByteString.Lazy as L
 import Data.Csv
 import Data.Maybe (fromJust, isJust)
-import Epidemic
 import Epidemic.Types.Population
 import Epidemic.Types.Events
 import qualified Epidemic.BDSCOD as BDSCOD
@@ -20,13 +19,9 @@ main =
           (2.5, 0.2, 0.15, [(3, 0.5), (4, 0.5)], 0.2, [(3.5, 0.5)])
    in if isJust simConfig
          then do events <- EpiUtil.simulation True (fromJust simConfig) BDSCOD.allEvents
-                 print events
-                 print (BDSCOD.observedEvents events)
-                 print $ maybeEpidemicTree (BDSCOD.observedEvents events)
-                 let newickBuilder = asNewickString (0, Person 1) =<< maybeEpidemicTree (BDSCOD.observedEvents events)
-                 print $ BBuilder.toLazyByteString <$> newickBuilder
-                 -- L.writeFile "demo-newick-string.txt" ()
-                 -- L.writeFile "demo-output-all-events.csv" (encode events)
+                 let Just (newickBuilder,newickMetaData) = asNewickString (0, Person 1) =<< maybeEpidemicTree events
+                 L.writeFile "demo-newick-string.txt" $ BBuilder.toLazyByteString newickBuilder
+                 L.writeFile "demo-newick-metadata.csv" $ encode newickMetaData
                  -- L.writeFile "demo-output-observed-events.csv" . encode $
                    -- BDSCOD.observedEvents events
                  return ()

8ea1620383671aee74a03896ee4997599c7c870b
Alex Zarebski <aezarebski@gmail.com>
2021-02-05 18:00:02 +0000

    define a random event

--- src/Epidemic/Model/LogisticBDSD.hs
+++ src/Epidemic/Model/LogisticBDSD.hs
@@ -9,26 +9,45 @@ module Epidemic.Model.LogisticBDSD
   , LogisticBDSDPopulation(..)
   ) where
 
+import Data.Maybe (fromJust)
 import qualified Data.Vector as V
+import qualified Data.Vector.Generic as G
+import Epidemic (noScheduledEvent, firstScheduled)
 import Epidemic.Types.Events (EpidemicEvent(..))
 import Epidemic.Types.Parameter
   ( AbsoluteTime(..)
   , ModelParameters(..)
   , Probability
   , Rate
-  , TimeDelta
+  , TimeDelta(..)
   , Timed(..)
   , asTimed
+  , timeAfterDelta
   )
-import Epidemic.Types.Population (Identifier(..), People(..), Population(..), nullPeople, numPeople)
+import Epidemic.Types.Population
+  ( Identifier(..)
+  , People(..)
+  , Population(..)
+  , addPerson
+  , nullPeople
+  , numPeople
+  )
 import Epidemic.Types.Simulation
   ( SimulationConfiguration(..)
   , SimulationRandEvent(..)
   , SimulationState(..)
   )
-import Epidemic.Utility (initialIdentifier, maybeToRight, newPerson)
+import Epidemic.Utility
+  ( initialIdentifier
+  , maybeToRight
+  , newPerson
+  , randomPerson
+  )
 import System.Random.MWC (GenIO)
+import System.Random.MWC.Distributions (bernoulli, categorical, exponential)
 
+-- | The parameters of the logistic-BDSD process. This process allows for
+-- infections, removals, sampling and disasters.
 data LogisticBDSDParameters =
   LogisticBDSDParameters
     { paramsBirthRate :: Rate
@@ -112,8 +131,45 @@ randEvent' ::
   -> Identifier
   -> GenIO
   -> IO (AbsoluteTime, EpidemicEvent, LogisticBDSDPopulation, Identifier)
-randEvent' = undefined
+randEvent' params@LogisticBDSDParameters{..} currTime currPop@(LogisticBDSDPopulation currPpl) currId gen =
+  let netEventRate = (fromJust $ eventRate currPop params currTime)
+      popSizeDouble = fromIntegral $ numPeople currPpl
+      logisticBR = logisticBirthRate params currPop
+      eventWeights = V.fromList [logisticBR, paramsDeathRate, paramsSamplingRate]
+      in do delay <- exponential (netEventRate * popSizeDouble) gen
+            let newEventTime = timeAfterDelta currTime (TimeDelta delay)
+            if noScheduledEvent currTime newEventTime paramsDisasters
+              then do eventIx <- categorical eventWeights gen
+                      (randPerson, otherPeople) <- randomPerson currPpl gen
+                      return $ case eventIx of
+                        0 -> let (infectedPerson, newId) = newPerson currId
+                                 infEvent = Infection newEventTime randPerson infectedPerson
+                                 newPop = LogisticBDSDPopulation (addPerson infectedPerson currPpl)
+                                 in ( newEventTime
+                                    , infEvent
+                                    , newPop
+                                    , newId)
+                        1 -> (newEventTime, Removal newEventTime randPerson, LogisticBDSDPopulation otherPeople, currId)
+                        2 -> (newEventTime, Sampling newEventTime randPerson, LogisticBDSDPopulation otherPeople, currId)
+                        _ -> error "do not recognise the type of event index."
+              else let (Just dsstr@(dsstrTime, _)) = firstScheduled currTime paramsDisasters
+                   in do (schdEvent,postEventPpl) <- randomDisasterEvent dsstr currPop gen
+                         return (dsstrTime, schdEvent, postEventPpl, currId)
 
+-- | Return a randomly sampled Disaster event
+-- TODO Move this into the epidemic module to keep things DRY.
+randomDisasterEvent :: (AbsoluteTime,Probability) -- ^ Time and probability of sampling in the disaster
+                    -> LogisticBDSDPopulation    -- ^ The state of the population prior to the disaster
+                    -> GenIO
+                    -> IO (EpidemicEvent,LogisticBDSDPopulation)
+randomDisasterEvent (dsstrTime, dsstrProb) (LogisticBDSDPopulation (People currPpl)) gen = do
+  randBernoullis <- G.replicateM (V.length currPpl) (bernoulli dsstrProb gen)
+  let filterZip predicate a b = fst . V.unzip . V.filter predicate $ V.zip a b
+      sampledPeople = filterZip snd currPpl randBernoullis
+      unsampledPeople = filterZip (not . snd) currPpl randBernoullis
+   in return
+        ( Disaster dsstrTime (People sampledPeople)
+        , LogisticBDSDPopulation (People unsampledPeople))
 
 -- | From a full list of epidemic events that occurred in a simulation return
 -- just the ones that were observed. For the logistic-BDSD this is those that

fc1e6645c9e6315f678ceaaedb73b09a364b04c7
aezarebski <aezarebski@gmail.com>
2021-02-16 10:17:00 +0000

    set up to have the observedevents function in the observations module

--- src/Epidemic/Model/LogisticBDSD.hs
+++ src/Epidemic/Model/LogisticBDSD.hs
@@ -3,7 +3,6 @@
 
 module Epidemic.Model.LogisticBDSD
   ( configuration
-  , observedEvents
   , randomEvent
   , LogisticBDSDParameters(..)
   , LogisticBDSDPopulation(..)
@@ -170,11 +169,3 @@ randomDisasterEvent (dsstrTime, dsstrProb) (LogisticBDSDPopulation (People currP
    in return
         ( Disaster dsstrTime (People sampledPeople)
         , LogisticBDSDPopulation (People unsampledPeople))
-
--- | From a full list of epidemic events that occurred in a simulation return
--- just the ones that were observed. For the logistic-BDSD this is those that
--- are part of the reconstructed tree and those that occurred in disasters. If
--- this is not possible return an error message in a string.
--- TODO Implement this function
-observedEvents :: [EpidemicEvent] -> Either String [EpidemicEvent]
-observedEvents = undefined
--- src/Epidemic/Types/Observations.hs
+++ src/Epidemic/Types/Observations.hs
@@ -1,2 +1,23 @@
+{-# LANGUAGE DeriveGeneric #-}
 
-module Epidemic.Types.Observations where
+module Epidemic.Types.Observations
+  ( Observation(..)
+  , observedEvents
+  ) where
+
+import qualified Data.Aeson as Json
+import Epidemic.Types.Events (EpidemicEvent(..))
+import GHC.Generics
+
+-- | A wrapper for an 'EpidemicEvent' to indicate that this is an even that was
+-- observed rather than just an event of the epidemic process.
+newtype Observation =
+  Observation EpidemicEvent
+  deriving (Show, Eq, Generic)
+
+instance Json.FromJSON Observation
+
+instance Json.ToJSON Observation
+
+observedEvents :: [EpidemicEvent] -> Either String [Observation]
+observedEvents = undefined

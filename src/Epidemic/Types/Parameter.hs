9264c7992ac4941867e07db34a49224bb8db5e2c
aezarebski <aezarebski@gmail.com>
2021-03-11 11:21:11 +0000

    docs

--- src/Epidemic/Types/Parameter.hs
+++ src/Epidemic/Types/Parameter.hs
@@ -10,9 +10,21 @@ import Epidemic.Types.Time (AbsoluteTime(..))
 -- | Class of types that can be considered parameterisations of a epidemic
 -- model.
 class (Population p) => ModelParameters a p where
+
+  -- | The basic reproduction number.
+  --
+  -- __NOTE__ that this is not the /effective/ reproduction number the
+  -- population is included in case there is structure other than immunity that
+  -- needs to be accounted for.
   rNaught :: p -> a -> AbsoluteTime -> Maybe Double
+
+  -- | The total event rate at a particular point in time.
   eventRate :: p -> a -> AbsoluteTime -> Maybe Rate
+
+  -- | The probability that an event will result in an infection.
   birthProb :: p -> a -> AbsoluteTime -> Maybe Probability
+
+  -- | The __unnormalised__ distribution across the possible events.
   eventWeights :: p -> a -> AbsoluteTime -> Maybe (Vector Double)
 
 type Rate = Double

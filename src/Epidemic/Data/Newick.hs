cbb1134f6ffba93ce42f10bf86a83faf16785b9b
Alex Zarebski <aezarebski@gmail.com>
2021-07-20 14:44:36 +0100

    update newick export to work with burrs

--- src/Epidemic/Data/Newick.hs
+++ src/Epidemic/Data/Newick.hs
@@ -16,61 +16,50 @@ class Newick t
   asNewickString ::
        (AbsoluteTime, Person) -- ^ The person and time of the root of the tree
     -> t
-    -> Either String (BBuilder.Builder, [EpidemicEvent])
+    -> Either String BBuilder.Builder
 
-ampersandBuilder :: BBuilder.Builder
-ampersandBuilder = BBuilder.charUtf8 '&'
+bb :: Char -> BBuilder.Builder
+bb = BBuilder.charUtf8
 
-colonBuilder :: BBuilder.Builder
-colonBuilder = BBuilder.charUtf8 ':'
-
-leftBraceBuilder :: BBuilder.Builder
-leftBraceBuilder = BBuilder.charUtf8 '('
-
-rightBraceBuilder :: BBuilder.Builder
-rightBraceBuilder = BBuilder.charUtf8 ')'
-
-commaBuilder :: BBuilder.Builder
-commaBuilder = BBuilder.charUtf8 ','
+branchLengthBuilder :: AbsoluteTime -> AbsoluteTime -> BBuilder.Builder
+branchLengthBuilder t1 t2 =
+  let (TimeDelta td) = timeDelta t1 t2
+  in BBuilder.doubleDec td
 
 catastrophePeopleBuilder :: People -> BBuilder.Builder
 catastrophePeopleBuilder (People ps) =
-  mconcat . List.intersperse ampersandBuilder $ map personByteString pList
+  mconcat . List.intersperse (bb '&') $ map personByteString pList
   where pList = Set.toList ps
 
 instance Newick ReconstructedTree where
   asNewickString (t, _) (RLeaf (Observation e)) =
-    let branchLength a b = BBuilder.doubleDec td
-          where
-            (TimeDelta td) = timeDelta a b
-     in case e of
-          IndividualSample {..} ->
-            if indSampSeq
-              then return
-                     ( personByteString indSampPerson <>
-                       colonBuilder <> branchLength t indSampTime
-                     , [e])
-              else Left $ "non-sequenced individual sample in reconstructed tree: " <> show e
-          PopulationSample {..} ->
-            if popSampSeq
-              then return
-                     ( catastrophePeopleBuilder popSampPeople <>
-                       colonBuilder <> branchLength t popSampTime
-                     , [e])
-              else Left $ "non-sequenced population sample in reconstructed tree: " <> show e
-          _ -> Left $ "leaf of reconstructed tree does not contain a sample: " <> show e
+    case e of
+      IndividualSample {..} ->
+        if indSampSeq
+        then return $ personByteString indSampPerson <> bb ':' <> branchLengthBuilder t indSampTime
+        else Left $ "non-sequenced individual sample in reconstructed tree: " <> show e
+      PopulationSample {..} ->
+        if popSampSeq
+        then return $ catastrophePeopleBuilder popSampPeople <> bb ':' <> branchLengthBuilder t popSampTime
+        else Left $ "non-sequenced population sample in reconstructed tree: " <> show e
+      _ -> Left $ "leaf of reconstructed tree does not contain a sample: " <> show e
   asNewickString (t, _) (RBranch (Observation e) lt rt) =
     case e of
       (Infection t' p1 p2) -> do
-        (leftNS, leftEs) <- asNewickString (t', p1) lt
-        (rightNS, rightEs) <- asNewickString (t', p2) rt
-        let branchLength = BBuilder.doubleDec td
-              where
-                (TimeDelta td) = timeDelta t t'
-        return
-          ( leftBraceBuilder <>
-            leftNS <>
-            commaBuilder <>
-            rightNS <> rightBraceBuilder <> colonBuilder <> branchLength
-          , List.sort $ leftEs ++ rightEs)
+        leftNS <- asNewickString (t', p1) lt
+        rightNS <- asNewickString (t', p2) rt
+        let branchLength = branchLengthBuilder t t'
+        return $ bb '(' <> leftNS <> bb ',' <> rightNS <> bb ')' <> bb ':' <> branchLength
       _ -> Left $ "branch of reconstructed tree does not contain an infection: " <> show e
+  asNewickString (t, _) (RBurr (Observation e) mt) =
+    case e of
+      IndividualSample {..} ->
+        let branchLength = branchLengthBuilder t indSampTime
+            idColonLength = personByteString indSampPerson <> bb ':' <> branchLength
+        in case mt of
+             Just reconT ->
+               do tNS <- asNewickString (indSampTime, indSampPerson) reconT
+                  return $ bb '(' <> tNS <> bb ')' <> idColonLength
+             Nothing ->
+               return idColonLength
+      _ -> Left $ "burr of reconstructed tree does not contain an individual sample: " <> show e
